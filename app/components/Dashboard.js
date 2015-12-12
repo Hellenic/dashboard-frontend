42242d2cc81f69cf9a65f4d8194a539fe3d02ca0
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2015-12-12 21:25:10 +0200

    Changed Gridster to React Grid Layout

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -9,13 +9,14 @@ var FlagDaysWidget = require('./widgets/FlagDaysWidget/FlagDaysWidget.js');
 
 var Dashboard = React.createClass({
   getInitialState: function() {
+    // TODO Get this from localStorage
     return {
-      widgets: [ NewsWidget, LunchWidget, FlagDaysWidget ]
+      widgets: [NewsWidget, LunchWidget, FlagDaysWidget],
+      layout: [{ x: 0, y: 0, w: 2, h: 1, i: 1 }, { x: 2, y: 0, w: 2, h: 1, i: 2}, { x: 4, y: 0, w: 4, h: 2, i: 3}]
     };
   },
   componentDidMount: function() {
-    // Do stuff on-load
-    // e.g. Should get available components from localStorage
+
   },
   render: DashboardTemplate
 });
--- app/main.js
+++ app/main.js
@@ -7,19 +7,3 @@ var routes = require('./routes/routes.js');
 var Router = ReactRouter.Router;
 
 React.render(<Router routes={routes}/>, document.getElementById("app"));
-
-$(document).ready(function() {
-
-  $(".gridster").gridster({
-    widget_selector: "figure",
-    widget_margins: [10, 10],
-    widget_base_dimensions: [200, 200],
-    min_cols: 2,
-    max_cols: 16,
-    min_rows: 2,
-    resize: {
-      enabled: true
-    }
-  });
-
-});

2592620beeb8784352710e80c4256847523b6d86
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2015-12-13 17:58:06 +0200

    Added TODO widget for upcoming plans

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -6,17 +6,15 @@ var DashboardTemplate = require('./Dashboard.rt.js');
 var NewsWidget = require('./widgets/NewsWidget/NewsWidget.js');
 var LunchWidget = require('./widgets/LunchWidget/LunchWidget.js');
 var FlagDaysWidget = require('./widgets/FlagDaysWidget/FlagDaysWidget.js');
+var TodoListWidget = require('./widgets/TodoListWidget/TodoListWidget.js');
 
 var Dashboard = React.createClass({
   getInitialState: function() {
     // TODO Get this from localStorage
     return {
-      widgets: [NewsWidget, LunchWidget, FlagDaysWidget],
-      layout: [{ x: 0, y: 0, w: 4, h: 3, i: 1 }, { x: 0, y: 4, w: 4, h: 3, i: 2}, { x: 5, y: 0, w: 8, h: 6, i: 3}]
+      widgets: [NewsWidget, LunchWidget, FlagDaysWidget, TodoListWidget],
+      layout: [{ x: 0, y: 0, w: 4, h: 3, i: 1 }, { x: 0, y: 4, w: 4, h: 3, i: 2}, { x: 5, y: 0, w: 8, h: 6, i: 3}, { x: 2, y: 7, w: 4, h: 3, i: 4}]
     };
-  },
-  componentDidMount: function() {
-
   },
   render: DashboardTemplate
 });
---
+++ app/components/widgets/TodoListWidget/TodoListWidget.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var React = require('react');
+var WidgetTemplate = require('./TodoListWidget.rt.js');
+
+var TodoListWidget = React.createClass({
+  render: WidgetTemplate
+});
+
+module.exports = TodoListWidget;

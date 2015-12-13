ba7b794f40d2b6547bab786a4371937a0a142508
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2015-12-13 11:25:53 +0200

    Added LESS for Gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,31 @@
-var gulp       = require('gulp');
+var gulp = require('gulp');
 var browserify = require('gulp-browserify');
+var less = require('gulp-less');
 var rt = require('gulp-react-templates');
-
-gulp.task('scripts', function () {
-    gulp.src(['app/main.js'])
-        .pipe(browserify({
-            debug: true,
-            transform: [ 'reactify' ]
-        }))
-        .pipe(gulp.dest('./public/'));
-});
+var path = require('path');
 
 gulp.task('rt', function() {
-    gulp.src('app/components/**/*.rt')
-        .pipe(rt({ modules: 'commonjs', format: 'stylish' }))
-        .pipe(gulp.dest('app/components/'));
+  gulp.src('app/components/**/*.rt')
+    .pipe(rt({ modules: 'commonjs', format: 'stylish' }))
+    .pipe(gulp.dest('app/components/'));
 });
 
-gulp.task('default', ['scripts', 'rt']);
+gulp.task('scripts', function () {
+  gulp.src(['app/main.js'])
+    .pipe(browserify({
+        debug: true,
+        transform: [ 'reactify' ]
+    }))
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('less', function () {
+  gulp.src('app/less/main.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ],
+      compress: false
+    }))
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('default', ['rt', 'scripts', 'less']);

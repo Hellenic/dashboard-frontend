0a770ec0e464614cfc74268609785da10ac84955
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2016-01-07 17:05:51 +0200

    Added Helmet for server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,16 @@
 
 var express = require('express');
 var engines = require('consolidate');
+var helmet = require('helmet')
+
 var app = express();
 var port = 4444;
 
 require('babel-register');
 
-// Include static assets. Not advised for production
+// Include static assets
 app.use(express.static('public/'));
+app.use(helmet());
 // Set view path
 app.set('views', 'public/');
 // set up HTML + Mustache for templating.

8c710fe907a3768eb8be8cba2f122633cdf13461
Hannu Kärkkäinen <hannu.karkkainen@gmail.com>
2016-01-26 19:47:09 +0200

    Changed logic to fetch places from the dashboard-backend

--- app/components/widgets/OpeningTimesWidget/OpeningTimesWidget.js
+++ app/components/widgets/OpeningTimesWidget/OpeningTimesWidget.js
@@ -2,101 +2,30 @@
 
 var React = require('react');
 var moment = require('moment');
+var Ajax = require('../../utils/Ajax');
 var OpeningTimesWidgetTemplate = require('./OpeningTimesWidget.rt.js');
 
 var OpeningTimesWidget = React.createClass({
   getDefaultProps: function() {
     return {
-      id: 'OpeningTimesWidget'
+      id: 'OpeningTimesWidget',
+      url: 'http://0.0.0.0:3000/api/Places'
     };
   },
   getInitialState: function() {
     return {
-      places: [{
-        "name": "Leppävaaran uimahalli",
-        "openingTimes": [
-          {
-            "weekday": "Monday",
-            "from": "7:00",
-            "to": "20:00"
-          },
-          {
-            "weekday": "Tuesday",
-            "from": "6:00",
-            "to": "20:00"
-          },
-          {
-            "weekday": "Wednesday",
-            "from": "10:00",
-            "to": "20:00"
-          },
-          {
-            "weekday": "Thursday",
-            "from": "6:00",
-            "to": "20:00"
-          },
-          {
-            "weekday": "Friday",
-            "from": "7:00",
-            "to": "20:00"
-          },
-          {
-            "weekday": "Saturday",
-            "from": "9:00",
-            "to": "19:00"
-          },
-          {
-            "weekday": "Sunday",
-            "from": "9:00",
-            "to": "19:00"
-          }
-        ]
-      },{
-          "name": "Cafe Zoceria",
-          "openingTimes": [
-            {
-              "weekday": "Monday",
-              "from": "8:00",
-              "to": "19:00"
-            },
-            {
-              "weekday": "Tuesday",
-              "from": "8:00",
-              "to": "19:00"
-            },
-            {
-              "weekday": "Wednesday",
-              "from": "8:00",
-              "to": "19:00"
-            },
-            {
-              "weekday": "Thursday",
-              "from": "8:00",
-              "to": "19:00"
-            },
-            {
-              "weekday": "Friday",
-              "from": "8:00",
-              "to": "19:00"
-            },
-            {
-              "weekday": "Saturday",
-              "from": "10:00",
-              "to": "18:00"
-            },
-            {
-              "weekday": "Sunday",
-              "from": "11:00",
-              "to": "16:00"
-            }
-          ]
-        }
-      ]
+      places: []
     };
   },
   isToday: function(openingTime) {
     let day = moment().format('dddd');
     return (day == openingTime.weekday);
+  },
+  handleResponse: function(data) {
+    this.setState({ places: data });
+  },
+  componentDidMount: function() {
+    Ajax.get(this.props.url, this.handleResponse);
   },
   render: OpeningTimesWidgetTemplate
 });
